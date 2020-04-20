5ec68db7d83d87878c2461f5b3476980bf117861
Ryan Yang <RyanYang.nz@outlook.com>
2020-04-20 21:55:04 +1200

    Set up DbUp to keep track of SQL Script.

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DbUp;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,24 @@ namespace QandA
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges.To
+              .SqlDatabase(connectionString, null)
+              .WithScriptsEmbeddedInAssembly(
+                System.Reflection.Assembly.GetExecutingAssembly()
+              )
+              .WithTransaction()
+              .LogToConsole()
+              .Build();
+
+            if (upgrader.IsUpgradeRequired())
+            {
+                upgrader.PerformUpgrade();
+            }
+
             services.AddControllers();
         }
 
