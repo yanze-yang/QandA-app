d5e592b2d9ae1fdc6c522742ef60581814bb62b5
Ryan Yang <RyanYang.nz@outlook.com>
2020-05-13 16:31:08 +1200

    Build Question And Anwers APIs.

--- backend/Data/Models/QuestionPostRequest.cs
+++ backend/Data/Models/QuestionPostRequest.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace QandA.Data.Models
 {
     public class QuestionPostRequest
     {
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please include some content for the question")]
         public string Content { get; set; }
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public DateTime Created { get; set; }
     }
 }
